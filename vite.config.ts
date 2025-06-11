e33fdd1bcdb7c39852dc57d4c43544d996821062
Vahizan <vahizanv@gmail.com>
2025-06-12 00:13:48 +0100

    separate frontend and backend build

--- vite.config.ts
+++ vite.config.ts
@@ -40,7 +40,7 @@ export default defineConfig({
   },
   root: path.resolve(__dirname, "client"),
   build: {
-    outDir: path.resolve(__dirname, "dist/public"),
+    outDir: path.resolve(__dirname, "dist/client"),
     emptyOutDir: true,
   },
 });
